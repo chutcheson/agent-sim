46ad08bc18b2a104eb35e8427029bfd62130341d
Campbell Hutcheson <nchutcheson@gmail.com>
2023-06-30 09:32:05 -0400

    update strategy

--- agent_sim/strategist.py
+++ agent_sim/strategist.py
@@ -13,16 +13,17 @@ class Strategist:
         self.model = model
         self.role = role
         self.goal = goal
-        self.thoughts = "This is the start of the conversation. I should lay out the background."
+        self.thoughts = ["This is the start of the conversation. I should lay out the background."]
 
     def strategize(self, previous_conversation):
+        merged_thoughts = "\n".join([f"Thought {i}: {thought}" for i, thought in enumerate(self.thoughts)])
         llm_messages = [
             SystemMessage(content=STRATEGIST_SYSTEM_PROMPT.format(role=self.role, goal=self.goal)),
             HumanMessage(content=STRATEGIST_USER_PROMPT.format(
                 conversation=previous_conversation,
-                thoughts=self.thoughts))
+                thoughts=merged_thoughts))
         ]
         response = self.model.predict_messages(llm_messages).content
-        self.thoughts += response
+        self.thoughts.append(response)
         return response
 
